3227e3cbafb08841bee6cf68e10cdd9f13e505db
Todd Hiles <todd@bugslifesolutions.com>
2023-05-20 22:32:39 +0000

    fix: unmet peer graphql@^15.5.3: found 14.7.0

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import policies from "./policies.json";
 import startup from "./startup.js";
 import registerPluginHandler from "./registration.js";
 
+/** todo: refactor to make easier to add watching and indexing other collections.
+ * maybe add a graphQL resolver with a mutation for a 'watch and index' responsibility; 'IndexSynchronizer' 'ListeningElasticIndexer' 'EventStash'.
+ * EventStash -
+ * {
+ *   reaction_event_name: 'afterPublishProductToCatalog',
+ *   documentTransform: function(context, document) returns object
+ *   stash: function(document)
+ * }
+ * Helper ElasticStash function (indexName, optional client config) returns a configured stash(object) function
+ */
+
 /**
  * @summary Import and call this function to add this plugin to your API.
  * @param {ReactionAPI} app The ReactionAPI instance
