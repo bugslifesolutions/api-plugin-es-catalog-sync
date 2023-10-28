5617a8c1577aad746d1d6141fbfd4eaedc6ad354
Todd Hiles <todd@bugslifesolutions.com>
2023-10-28 20:06:21 +0000

    fix: nodejs import assertions babel

--- babel.config.cjs
+++ babel.config.cjs
@@ -5,25 +5,25 @@
  */
 
 module.exports = function (api) { // eslint-disable-line no-undef
-    api.cache(false);
-  
-    return {
-      presets: [
-        [
-          "@babel/preset-env",
-          {
-            targets: {
-              node: "18"
-            }
+  api.cache(false);
+
+  return {
+    presets: [
+      [
+        "@babel/preset-env",
+        {
+          targets: {
+            node: "18"
           }
-        ]
-      ],
-      plugins: [
-        "babel-plugin-transform-import-meta",
-        "module:@reactioncommerce/babel-remove-es-create-require",
-        "rewire-exports",
-        "transform-es2015-modules-commonjs"
+        }
       ]
-    };
+    ],
+    plugins: [
+      "babel-plugin-transform-import-meta",
+      "@babel/plugin-syntax-import-assertions",
+      "module:@reactioncommerce/babel-remove-es-create-require",
+      "rewire-exports",
+      "transform-es2015-modules-commonjs"
+    ]
   };
-  
+};
